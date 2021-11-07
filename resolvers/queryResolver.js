ce78485da52a16b0ef0ab44b724f7626979cc1f5
benfonty <benfonty@gmail.com>
2021-11-07 19:59:36 +0100

    added workflow in command resolver

--- resolvers/queryResolver.js
+++ resolvers/queryResolver.js
@@ -1,3 +1,45 @@
+const commands = [
+    {
+        ownerId: "toto",
+        id: "titi",
+        createdAt: new Date().toISOString()
+    },
+    {
+        ownerId: "toto",
+        id: "toto",
+        createdAt: new Date().toISOString()
+    },
+    {
+        ownerId: "tutu",
+        id: "tutu",
+        createdAt: new Date().toISOString()
+    },
+];
+
+const workflows = {
+    "titi": [
+        {
+            id: "titi1",
+            startedAt: new Date().toISOString(),
+            endedAt: new Date().toISOString(),
+            operation: "PROVISION"
+        },
+        {
+            id: "titi2",
+            startedAt: new Date().toISOString(),
+            operation: "UPDATE"
+        }
+    ],
+    "toto": [
+        {
+            id: "toto1",
+            startedAt: new Date().toISOString(),
+            endedAt: new Date().toISOString(),
+            operation: "PROVISION"
+        },
+    ],
+};
+
 module.exports.resolver = {
     Query: {
         commands: (obj, args, context, info) => {
@@ -5,17 +47,19 @@ module.exports.resolver = {
             console.log(args);
             console.log(context)
             //console.log(info);
-            if (args.ownerId && args.ownerId === "titi") {
-                return [
-                    {
-                        ownerId: "toto",
-                        id: "titi",
-                        createdAt: new Date().toISOString()
-                    }
-                ];
-            } else {
-                return [];
-            }
+            let response = commands;
+            if (args.ownerId) {
+                response = response.filter(v => v.ownerId === args.ownerId);
+            } 
+            return response;
+        }
+    },
+    Command: {
+        workflows: (obj, args, context, info) => {
+            console.log(obj);
+            console.log(args);
+            console.log(context)
+            return workflows[obj.id].map(v => Object.assign({}, v, { commandId: obj.id }));
         }
     }
 };
